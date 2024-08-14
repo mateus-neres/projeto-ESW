65f9d8906c62915f76805a63197a861df2d46e24
Mateus Neres <mateusneres03@gmail.com>
2024-08-14 01:22:52 -0300

    Tratamento de dados

--- arquivos/biblioteca.py
+++ arquivos/biblioteca.py
@@ -3,62 +3,62 @@ from openpyxl import Workbook
 import os
 import tabula
 import PyPDF2
+import pandas as pd
 
-# Recebe o diretório e o nome do arquivo PDF e retorna uma tabela
-def pdf_para_tabela(caminho_pdf, nome_pdf):
-    logger.info('Iniciando a conversão do PDF para tabela.')
-    caminho_completo = os.path.join(caminho_pdf, nome_pdf)
+# Recebe o diretório e o nome do arquivo PDF e retorna uma lista de tabelas extraídas
+def pdf_para_tabela(diretorio_pdf, nome_pdf):
+    logger.info('Iniciando a conversão do PDF para tabelas.')
+    caminho_pdf = os.path.join(diretorio_pdf, nome_pdf)
     
     try:
-        tabelas_extraidas = tabula.read_pdf(caminho_completo, pages='all', stream=True, multiple_tables=True, encoding='ISO-8859-1')
+        tabelas = tabula.read_pdf(caminho_pdf, pages='all', stream=True, multiple_tables=True, encoding='ISO-8859-1')
         
-        if not tabelas_extraidas:
+        if not tabelas:
             logger.warning('Nenhuma tabela foi extraída do PDF.')
             return None
         
-        logger.info('Conversão do PDF para tabela concluída com sucesso.')
-        return tabelas_extraidas
+        logger.info('Conversão do PDF para tabelas concluída com sucesso.')
+        return tabelas
 
     except Exception as e:
-        logger.error(f'Erro ao converter PDF para tabela: {e}')
+        logger.error(f'Erro ao converter PDF para tabelas: {e}')
         return None
 
 
-# Recebe a tabela de dados, o diretório para salvar o arquivo Excel e o nome do arquivo, e cria um arquivo Excel
-def tabela_para_excel(tabelas, caminho_para_salvar_excel, nome_arquivo_excel):
-    logger.info('Iniciando a conversão da tabela para arquivo Excel.')
-    caminho_completo = os.path.join(caminho_para_salvar_excel, nome_arquivo_excel)
+# Recebe uma lista de tabelas, o diretório para salvar o arquivo Excel e o nome do arquivo, e cria um arquivo Excel
+def tabela_para_excel(tabelas, diretorio_excel, nome_arquivo_excel):
+    logger.info('Iniciando a conversão das tabelas para arquivo Excel.')
+    caminho_excel = os.path.join(diretorio_excel, nome_arquivo_excel)
     try:
         if not tabelas:
             logger.warning('Nenhuma tabela foi fornecida.')
             return None
         
-    
-        pasta_de_trabalho = Workbook()
-        planilha_ativa = pasta_de_trabalho.active
+        workbook = Workbook()
+        sheet = workbook.active
         
         for tabela in tabelas:
             linhas = tabela.values.tolist()
 
             for linha in linhas:
                 if linha:
-                    planilha_ativa.append(linha)
+                    sheet.append(linha)
                 else:
                     logger.warning('Linha vazia encontrada e ignorada.')
 
-        pasta_de_trabalho.save(caminho_completo)
-        logger.info(f'Arquivo Excel salvo com sucesso em: {caminho_completo}')
+        workbook.save(caminho_excel)
+        logger.info(f'Arquivo Excel salvo com sucesso em: {caminho_excel}')
 
     except Exception as e:
         logger.error(f'Erro ao salvar a tabela como Excel: {e}')
 
 
-# Recebe o caminho e nome do arquivo PDF e retorna o texto do PDF
-def ler_pdf(caminho_pdf, nome_pdf):
+# Recebe o caminho e o nome do arquivo PDF e retorna o texto extraído do PDF
+def ler_pdf(diretorio_pdf, nome_pdf):
     logger.info('Iniciando a leitura do PDF.')
-    caminho_completo = os.path.join(caminho_pdf, nome_pdf)
+    caminho_pdf = os.path.join(diretorio_pdf, nome_pdf)
     try:
-        with open(caminho_completo, 'rb') as arquivo_pdf:
+        with open(caminho_pdf, 'rb') as arquivo_pdf:
             leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
             texto = ''
             
@@ -73,13 +73,39 @@ def ler_pdf(caminho_pdf, nome_pdf):
         return None
 
 
-# Cria um arquivo de texto a partir do texto fornecido
-def criar_txt(texto, nome_arquivo_txt):
+# Cria um arquivo de texto a partir do conteúdo fornecido
+def criar_txt(conteudo, nome_arquivo_txt):
     logger.info('Iniciando a criação do arquivo de texto.')
     try:
-        with open(nome_arquivo_txt, 'w', encoding='utf-8') as arquivo:
-            arquivo.write(texto)
-        logger.info(f'Texto escrito com sucesso no arquivo: {nome_arquivo_txt}')
+        with open(nome_arquivo_txt, 'w', encoding='utf-8') as arquivo_txt:
+            arquivo_txt.write(conteudo)
+        logger.info(f'Conteúdo escrito com sucesso no arquivo: {nome_arquivo_txt}')
 
     except Exception as e:
         logger.error(f'Erro ao criar o arquivo de texto: {e}')
+
+# Ajusta o valor de acordo com o formato esperado
+def ajustar_valor(valor):
+    if pd.isna(valor):
+        logger.info('Valor é NaN, retornando None.')
+        return None
+    
+    valor_str = str(valor).strip()
+    
+    valor_str = valor_str.replace('.', '').replace(' ', '')
+    
+    if valor_str.endswith('D') or valor_str.endswith('C'):
+        sufixo = valor_str[-1]
+        valor_str = valor_str[:-1]
+    else:
+        sufixo = ''
+    
+    try:
+        valor_ajustado = float(valor_str.replace(',', '.'))
+        if sufixo == 'D':
+            valor_ajustado = -valor_ajustado
+        logger.info(f'Valor ajustado: {valor_ajustado}')
+        return valor_ajustado
+    except ValueError:
+        logger.error(f'Valor inválido para conversão: {valor_str}')
+        return None
