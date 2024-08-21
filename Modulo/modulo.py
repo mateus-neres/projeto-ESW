11af2e093b5951f83668e9aebbd6e5e381c299ec
John Wesley <john.silva@dcx.ufpb.br>
2024-08-21 01:24:41 -0300

    Test

--- Modulo/modulo.py
+++ Modulo/modulo.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 # Definindo o diretório atual
-os.chdir(os.path.dirname(__file__))
+
 dir_atual = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 # Código principal
